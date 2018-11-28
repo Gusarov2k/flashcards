c23e221ea638d63bedcfe21324b36099c3d489e7
gusarov2k <motoface@yandex.ru>
2018-11-28 16:50:34 +0300

    task one with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'flash_cards#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
