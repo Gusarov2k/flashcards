94c37d3a04538cbf249ac63959d8d570ef58ea15
gusarov2k <motoface@yandex.ru>
2018-11-30 20:02:24 +0300

    create simple_form

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  validate :change_string
+
+  def change_string
+    if original_text == translated_text
+      errors.add(:original_text, 'Оригинальный текст не может быть равен переведенному')
+    end
+  end
 end
