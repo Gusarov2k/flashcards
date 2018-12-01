8725c215cc6df981dbb04990e6461460525a1f03
gusarov2k <motoface@yandex.ru>
2018-12-01 11:36:50 +0300

    add callbacks and fix link_to if use method destroy

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,20 @@
 class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, :review_date, presence: true
+  validates :original_text, :translated_text, presence: true
 
   validate :change_string
+
+  before_create :create_date
+
+
+  private
 
   def change_string
     if original_text == translated_text
       errors.add(:original_text, 'Оригинальный текст не может быть равен переведенному')
     end
+  end
+
+  def create_date
+    self.review_date = (Date.today + 3.days).to_s
   end
 end
