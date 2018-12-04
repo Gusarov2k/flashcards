428a17acdcf94928fecaa7ac7f15f02e7a453a1c
gusarov2k <motoface@yandex.ru>
2018-12-04 18:26:16 +0300

    parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# ! /usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+# require 'openssl'
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# Fetch and parse HTML document
+doc = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
+
+content = []
+
+doc.xpath('//tr').each do |value|
+  first_word = value.xpath('td[2]').text
+  second_word = value.xpath('td[3]').text
+  content.push([
+                 first_word,
+                 second_word
+               ])
+end
+
+content.each do |first, second|
+  Card.create(original_text: first, translated_text: second)
+end
