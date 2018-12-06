11e16cfb9873783dfc0e1204ebdb74e94d6dcf0c
gusarov2k <motoface@yandex.ru>
2018-12-06 16:55:55 +0300

    end of end

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,15 +4,11 @@ module Parser
     require 'open-uri'
 
     doc = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-german-words/'))
-    content = doc.xpath('//tr').each_with_object([]) do |value, massiv|
-      massiv.push([
-                    value.xpath('td[2]').text,
-                    value.xpath('td[3]').text
-                  ])
+    doc.xpath('//tr').each_with_object([]) do |value, arr|
+      arr.push([
+                 value.xpath('td[2]').text,
+                 value.xpath('td[3]').text
+               ])
     end
-  end
-
-  def self.hello
-    p 'hello'
   end
 end
