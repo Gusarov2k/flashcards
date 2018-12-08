d50011760fdc09482387edd280aaed0499a8cfbf
gusarov2k <motoface@yandex.ru>
2018-12-08 15:21:26 +0300

    ment

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -6,12 +6,21 @@ class FlashCardsController < ApplicationController
   def update; end
 
   def create
+    if flash_cards_params_original == flash_cards_params_user
+      redirect_to cards_path
+    else
+      render 'yt df'
+    end
 
   end
 
   private
 
-  def flash_cards_params
-    params.require(:card).permit(:translated_text)
+  def flash_cards_params_original
+    params.require(:flash_cards_value).permit!(:translated_text)
+  end
+
+  def flash_cards_params_user
+    params.require(:flash_cards_value).permit(:user_text)
   end
 end
