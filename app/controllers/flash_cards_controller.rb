458cdce2df2a291bcaeec36a023f5e69d2e660fe
gusarov2k <motoface@yandex.ru>
2018-12-09 17:06:55 +0300

    new test ss

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -1,25 +1,24 @@
 class FlashCardsController < ApplicationController
   def index
     @cards = Card.all.third_days_ago.sample(1)
+    @user_value = UserValidate.new
   end
 
   def update; end
 
   def create
-    if flash_cards_params_original == flash_cards_params_user
+    @user_value = UserValidate.new(flash_cards_params_user)
+
+    if @user_value.valid?
       redirect_to cards_path
     else
-      render 'yt df'
+      flash.now[:error] = "Can't connect..."
     end
   end
 
   private
 
-  def flash_cards_params_original
-    params.require(:flash_cards_value).permit!(:translated_text)
-  end
-
   def flash_cards_params_user
-    params.require(:flash_cards_value).permit(:user_text)
+    params.require(:user_validate).permit(:user_text, :original_text)
   end
 end
