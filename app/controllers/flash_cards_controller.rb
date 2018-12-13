c3e5a72971b89485a8cf8246caadb0d4fd690e04
gusarov2k <motoface@yandex.ru>
2018-12-13 09:11:48 +0300

    rebase

--- app/controllers/flash_cards_controller.rb
+++ app/controllers/flash_cards_controller.rb
@@ -2,12 +2,4 @@ class FlashCardsController < ApplicationController
   def index
     @cards = Card.all.third_days_ago
   end
-
-  def update; end
-
-  private
-
-  def flash_cards_params
-    params.require(:card).permit(:translated_text)
-  end
 end
