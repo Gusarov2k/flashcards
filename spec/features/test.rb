68989c9b86306f2dac9c649abd363aae351b3009
gusarov2k <motoface@yandex.ru>
2018-12-15 04:59:49 +0300

    test rspec 1

--- spec/features/test.rb
+++ spec/features/test.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe Card do
   it "opens courses page after login" do
-      expect(page).to have_content "Текущие курсы"
-    end
+    expect(page).to have_content "Текущие курсы"
+  end
 
   it "saves item record in DB" do
-    item = Card.create(...)
-    Card.find(card.id).should_not be_nil
+    item = Card.create(original_text: 'resr', translated_text: 'gest')
+    Card.find(item.id).should_not be_nil
   end
 end
