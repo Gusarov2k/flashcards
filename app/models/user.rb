079fdea97e95f0c9ed60c64a261fad1a7a823e9f
gusarov2k <motoface@yandex.ru>
2018-12-27 18:22:55 +0300

    start task 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
   has_many :cards
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :email, uniqueness: true
+
+
 end
