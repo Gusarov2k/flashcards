cd25f60196f48ba852a586cea867708ec99e7c8c
gusarov2k <motoface@yandex.ru>
2019-01-02 17:00:24 +0300

    add code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery
-  skip_before_action :require_login, only: %i[index new create]
+
+  before_action :require_login
 
   private
 
   def not_authenticated
-    redirect_to root_path, alert: 'Please login first.'
+    redirect_to log_in_path, alert: 'Please login first.'
   end
 end
